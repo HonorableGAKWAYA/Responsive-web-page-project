428bf3c281d5eea19fc4ee883a049a2ba8eca4f0
HonorableGAKWAYA <honorablegakwaya@gmail.com>
2021-06-24 00:15:42 +0200

    improve of design

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var toggleButton = document.getElementsByClassName("toggle-button")[0];
 var navLogs = document.getElementsByClassName("nav-logs")[0];
 
-//toggleButton.addEventListener("click", () =>{
-//    navLogs.classAnchors.toggle('display-flex')
-//})
+// toggleButton.addEventListener("click", () =>{
+//     navLogs.style. display ="flex";
+// })
 
-//function openToggleMenu (){
-  //  if (toggleButton, 'click'){
-    //    navLogs.style.display = "flex";
-    //}
-//}
+
+
+
+
+function openToggleMenu (){
+      navLogs.style.display = "flex";
+};
