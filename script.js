066063b3770c09408ef49b2365311a9d782dca19
HonorableGAKWAYA <honorablegakwaya@gmail.com>
2021-06-24 14:31:11 +0200

    active toggle-button

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 var toggleButton = document.getElementsByClassName("toggle-button")[0];
 var navLogs = document.getElementsByClassName("nav-logs")[0];
-
-// toggleButton.addEventListener("click", () =>{
-//     navLogs.style. display ="flex";
-// })
-
-
-
-
+var closeButton = document.getElementsByClassName("close-button")[0];
 
 function openToggleMenu (){
-      navLogs.style.display = "flex";
+      if ($(window).width() < 500) {
+            navLogs.style.visibility= 'visible';
+            toggleButton.style. display = "none";
+            closeButton.style. display = "flex";
+      }
+      else {
+            navLogs.style.visibility= 'visible';
+            toggleButton.style. display = "none";
+            closeButton.style. display = "none";
+      }
+      
+      
 };
+
+function closeToggleMenu (){
+      if ($(window).width() < 500) {
+            navLogs.style.visibility= 'hidden';
+            toggleButton.style. display = "flex";
+            closeButton.style. display = "none";
+      }
+      else {
+            navLogs.style.visibility= 'visible';
+            toggleButton.style. display = "none";
+            closeButton.style. display = "none";
+      }
+      
+      
+}; 
